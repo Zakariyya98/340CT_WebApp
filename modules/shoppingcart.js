a04182d46fd52eb2b45880bbc56373d40a87bc86
Zakariyya Ahmed <ahmedz27@coventry.ac.uk>
2019-12-01 22:29:32 +0000

    Refinements to the feature implemented.

--- modules/shoppingcart.js
+++ modules/shoppingcart.js
@@ -6,18 +6,18 @@ module.exports = class Cart {
 	constructor(dbCart = ':memory:') {
 		return (async() => {
 			this.db = await sqlite.open(dbCart)
-			const sql = 'CREATE TABLE IF NOT EXISTS cart (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price TEXT, op1 TEXT, op1tot TEXT, op2 TEXT, op2tot TEXT, op3 TEXT, op3tot TEXT);'
+			const sql = 'CREATE TABLE IF NOT EXISTS cart (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, price TEXT, op1 TEXT DEFAULT "N/A" , op1tot TEXT, op2 TEXT DEFAULT "N/A", op2tot TEXT, op3 TEXT DEFAULT "N/A", op3tot TEXT);'
 			await this.db.run(sql)
 			return this
 		})()
 	}
 
 	// eslint-disable-next-line max-params
-	async addtoCart(name, price, op1, op1tot, op2, op2tot, op3, op3tot) {
+	async addtoCart(name, price, op1, op1tot, op2, op2tot, op3, op3tot, picture) {
 		try{
 			let sql = `SELECT COUNT(id) as records FROM cart WHERE name="${name}"`
 			// eslint-disable-next-line max-len
-			sql = `INSERT INTO cart(name, price, op1, op1tot, op2, op2tot, op3, op3tot) VALUES("${name}", "${price}", "${op1}","${op1tot}","${op2}","${op2tot}","${op3}","${op3tot}")`
+			sql = `INSERT INTO cart(name, price, op1, op1tot, op2, op2tot, op3, op3tot, picture) VALUES("${name}", "${price}", "${op1}","${op1tot}","${op2}","${op2tot}","${op3}","${op3tot}", "${picture}")`
 			await this.db.run(sql)
 			return true
 
